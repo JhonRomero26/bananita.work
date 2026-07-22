0197252093fa8722bb3e26e3ccc5d6e5208ae6f9
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2026-07-22 18:22:40 -0500

    feat: build creative portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ana Paulina | Contenido, estrategia y diseño';
+export const SITE_DESCRIPTION =
+	'Portafolio de Ana Paulina Sarmiento, creadora de contenido y gestora de redes sociales en Loja, Ecuador.';
